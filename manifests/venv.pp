a0602a6567c0829ee8d68a8f7c85f6e7810b818d
Daniel Eriksson <daniel@starstable.com>
2014-05-16 22:18:04 +0000

    Umask set to the regular default umask

--- manifests/venv.pp
+++ manifests/venv.pp
@@ -1,7 +1,7 @@
 class python::venv($ensure=present,
                    $owner=undef,
                    $group=undef,
-                   $umask=0002) inherits python::dev {
+                   $umask=0022) inherits python::dev {
 
   package { "python-virtualenv":
     ensure => $ensure,
